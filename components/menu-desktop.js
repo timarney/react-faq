82d67205327206023302ae6ea3a41ce5c1796095
Tim Arney <tim@line37.com>
2017-02-28 05:25:18 -0500

    desktop nav + cleanup

---
+++ components/menu-desktop.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+import "../css/menu-desktop.css";
+import MenuItems from "../components/menu-items";
+
+class MenuDesktop extends Component {
+  render() {
+    return (
+      <div className="menu-desktop">
+        <MenuItems />
+      </div>
+    );
+  }
+}
+
+export default MenuDesktop;
