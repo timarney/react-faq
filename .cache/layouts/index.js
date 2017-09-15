c58f1dcc531b877512d404c58cc2078ed43e85f9
Tim Arney <tim@line37.com>
2017-09-15 17:02:45 -0400

    cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/timarney/Desktop/faq/react-faq/src/layouts/index.js"
-  import data from "/Users/timarney/Desktop/faq/react-faq/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
